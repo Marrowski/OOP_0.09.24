ce1d17976b6d5f65750eedfae5ab9eeb23aa7f54
Oleh Yemelianov <emelianov2811@gmail.com>
2024-09-11 16:03:47 +0300

    Second task completed

--- rectangle.py
+++ rectangle.py
@@ -8,23 +8,29 @@ class GraphicalObject:
 
 
 class Rectangle(GraphicalObject):
-    pass
+    def __init__(self, name: str, edges: int):
+        super().__init__(name, edges)
 
 
 class Object(GraphicalObject):
-    def button_method(self, button_click: str):
+    def button_method(self, button_click: str, button_status: bool):
         self.button_click = button_click
         button_click = input('Would you like to click the button:')
 
-        if button_click == 'Yes'.upper():
+        if button_click == 'Yes'.title():
             print('Button is now on pushed stage.')
-        elif button_click == 'No'.upper():
-            print('You havent pushed a button.')
+        elif button_click == 'No'.title():
+            print('You haven`t pushed a button.')
         else:
             print('Unknown action')
 
 
 class Button(Object):
-    def __init__(self, color: str, name: str, edges: int):
-        super().__init__(name, edges)
+    def button_method(self, button_click: str, button_status: bool):
+        return f'Button is {button_click}, button status {button_status}'
 
+
+object1 = GraphicalObject('Rectangle', 4)
+print(object1)
+object2 = Object('Triangle', 3)
+object2.button_method('Click', False)
